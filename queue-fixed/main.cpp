a1de91e99471d4a7a165076ff14e9278989242fd
duydang2412 <duydang2412@gmail.com>
2021-10-28 21:49:43 +0700

    feat(queue): add fixed queue implementation

---
+++ queue-fixed/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "./queue.cpp"
+
+int main() {
+    Queue<int>* queueA = new Queue<int>(4);
+    Queue<int>* queueB = new Queue<int>(3);
+
+    queueA->enqueue(1);
+    queueA->enqueue(2);
+    queueA->enqueue(3);
+    queueA->enqueue(4);
+    queueA->enqueue(5);
+
+    queueB->enqueue(7);
+    queueB->enqueue(8);
+    queueB->enqueue(9);
+
+    queueA->dequeue();
+
+    std::cout << "queue A: " << queueA->toString() << std::endl;
+    std::cout << "queue B: " << queueB->toString() << std::endl;
+    queueA->swap(queueB);
+    std::cout << "queue A: " << queueA->toString() << std::endl;
+    std::cout << "queue B: " << queueB->toString() << std::endl;
+    std::cout << "queue A: " << queueA->reverse()->toString() << std::endl;
+    std::cout << "queue B: " << queueB->reverse()->toString() << std::endl;
+
+    delete queueA;
+    delete queueB;
+    return 0;
+}
---
+++ queue-fixed/queue.cpp
@@ -0,0 +1,87 @@
+#include <string>
+#include "./queue.h"
+
+template<typename T> Queue<T>::Queue(std::size_t capacity) {
+    this->_capacity = capacity;
+    this->_rear = -1;
+    this->arr = new T[capacity];
+}
+
+template<typename T> Queue<T>::~Queue() {
+    delete this->arr;
+}
+
+template<typename T> T Queue<T>::front() const {
+    if(this->_rear == -1) {
+        return T();
+    }
+    return *(this->arr + 0);
+}
+
+template<typename T> T Queue<T>::rear() const {
+    if(this->_rear == -1) {
+        return T();
+    }
+    return *(this->arr + this->_rear);
+}
+
+template<typename T> bool Queue<T>::empty() const {
+    return this->_rear == -1;
+}
+
+template<typename T> std::size_t Queue<T>::size() const {
+    return this->_rear + 1;
+}
+
+template<typename T> std::size_t Queue<T>::capacity() const {
+    return this->_capacity;
+}
+
+template<typename T> Queue<T>* Queue<T>::enqueue(T value) {
+    if(this->_rear + 1 != this->_capacity) {
+        *(this->arr + ++this->_rear) = value;
+    }
+    return this;
+}
+
+template<typename T> Queue<T>* Queue<T>::dequeue() {
+    for(std::size_t i = 0; i != this->_rear; ++i) {
+        *(this->arr + i) = *(this->arr + i + 1);
+    }
+    --this->_rear;
+    return this;
+}
+
+template<typename T> Queue<T>* Queue<T>::swap(Queue<T>* stack) {
+    T* arr = this->arr;
+    this->arr = stack->arr;
+    stack->arr = arr;
+
+    T temp = this->_rear;
+    this->_rear = stack->_rear;
+    stack->_rear = temp;
+    
+    temp = this->_capacity;
+    this->_capacity = stack->_capacity;
+    stack->_capacity = temp;
+    return this;
+}
+
+template<typename T> std::string Queue<T>::toString() const {
+    std::string output = "";
+    for(std::size_t i = 0, size = this->_rear + 1; i != size; ++i) {
+        output += std::to_string(*(this->arr + i)) + " ";
+    }
+    output += (this->empty() ? ("(empty)") : ("(size " + std::to_string(this->size()) + ")"));
+    return output;
+}
+
+template<typename T> Queue<T>* Queue<T>::reverse() {
+    T temp;
+    for(std::size_t i = 0, size = std::size_t(this->size() / 2); i != size; ++i) {
+        temp = *(this->arr + i);
+        *(this->arr + i) = *(this->arr + this->_rear - i);
+        *(this->arr + this->_rear - i) = temp;
+    }
+    return this;
+}
